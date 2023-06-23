bde373480d4bba87665cc19d196e76b45ebdf9d7
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-06-23 11:37:10 +0530

    .

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] ='24ccf7efc0cae54f1bf927e7f59ee5e8f2' #unique secret key created by import os os.urandom(17),hex()
 
 #connecting to sql
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'#create sql database in the same directory 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)#sqlalchemy instance creation
-# model created in model.py file
-from __main__ import db
-class User(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(20),unique=True,nullable=False)
-    email=db.Column(db.String(120),unique=True,nullable=False)
-    img_file=db.Column(db.String(120),nullable=False,default="default.jpg")  #hash the image to string20
-    password=db.Column(db.String(60),nullable=False)#hashed string of 60 character
+# from flask_sqlalchemy import SQLAlchemy
+# app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'#create sql database in the same directory 
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)#sqlalchemy instance creation
+# # model created in model.py file
+# from __main__ import db
+# class User(db.Model):
+#     id=db.Column(db.Integer, primary_key=True)
+#     username=db.Column(db.String(20),unique=True,nullable=False)
+#     email=db.Column(db.String(120),unique=True,nullable=False)
+#     img_file=db.Column(db.String(120),nullable=False,default="default.jpg")  #hash the image to string20
+#     password=db.Column(db.String(60),nullable=False)#hashed string of 60 character
     
-    def __repr__(self):#used to specify how our object is going to be printed-> print(object)
-        return f"user('{self.username}','{self.email}','{self.img_file}')"
+#     def __repr__(self):#used to specify how our object is going to be printed-> print(object)
+#         return f"user('{self.username}','{self.email}','{self.img_file}')"
 
 # from model import User
 
